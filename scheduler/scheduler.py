e01be59612c7d64c20ff2d4133b19ea00dd56502
Carlos Gabriel Soares <carlos.soares@quintoandar.com.br>
2023-09-01 00:57:28 -0300

    refactor: remove unused lines

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -2,11 +2,9 @@ import schedule
 import time
 from datetime import datetime
 
-# Function to say hello
 def say_hello():
     print(f"Hello Sarue, it currently is {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
-# Schedule 'say_hello' function to run every day at 09:00 am
 schedule.every().day.at("00:53").do(say_hello)
 
 while True:
